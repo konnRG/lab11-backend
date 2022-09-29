5ecaf89c9253b6354b5a199172f0b7e6aa3fd357
konnRG <burussakorn_ruangon@elearning.cmu.ac.th>
2022-09-29 16:52:15 +0700

    1.2 Sensitive title query search.

--- src/main/java/se331/rest/dao/EventDao.java
+++ src/main/java/se331/rest/dao/EventDao.java
@@ -1,6 +1,7 @@
 package se331.rest.dao;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import se331.rest.entity.Event;
 
 import java.util.List;
@@ -9,7 +10,7 @@ public interface EventDao {
     Integer getEventSize();
     Page<Event> getEvents(Integer pageSize, Integer page);
     Event getEvent(Long id);
-
     Event save(Event event);
+    Page<Event> getEvent(String name, Pageable page);
 }
 
