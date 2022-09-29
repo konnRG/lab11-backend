8640d3ab79e29c3361755bcf76fe0a45e5490c5d
konnRG <burussakorn_ruangon@elearning.cmu.ac.th>
2022-09-29 16:54:22 +0700

    1.4 Added partial search.

--- src/main/java/se331/rest/repository/EventRepository.java
+++ src/main/java/se331/rest/repository/EventRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
     List<Event> findAll();
-    Page<Event> findByTitle(String title, Pageable pageRequest);
+    Page<Event> findByTitleContaining(String title, Pageable pageRequest);
 }
